2a23214c22b07fdf0ef1fb2fb5a1da523f576a77
InPanda <borderpanda@163.com>
2022-08-17 11:38:38 +0800

    core:修改webpack

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/CSS-IDEA',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
